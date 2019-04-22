5a1710b5febd1ce319d83dc8c27a32b9dfec3cb4
AdelineDb <adeline.dubosc@hotmail.fr>
2019-04-22 20:04:21 +0200

    ajout bouton accueil

--- unlink.php
+++ unlink.php
@@ -1,3 +1,15 @@
+<!doctype html>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Mon formulaire</title>
+
+</head>
+<body>
+
 <?php
 require 'treatmentForm.php';
 
@@ -7,4 +19,8 @@ if (isset($_POST)) {
 
     echo 'Le fichier a été correctement effacé';
 }
+?>
 
+<form action="index.php" method="post">
+    <input type="submit" value="Retourner à l'accueil">
+</form>
